8645b6852a621bebc3e4a367d5fa70304385d807
haeun <haeun9634@naver.com>
2025-10-08 19:42:06 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/example/momentory/MomentoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.momentory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
